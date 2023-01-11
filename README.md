b0471db166e3eb2b89abbd35db9f4b47d9ded3c2
Kevin Fasusi <kevin.fasusi@nhs.net>
2023-01-11 13:47:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Hypergraphs
+
+A library of animations explaining hypergraphs using manim.
+
+
+## Installation Instructions
+
+A detailed tutorial for installing manim can be found here:
+
+[Manim Docs](https://docs.manim.community/en/stable/installation.html)
+
+## Quick Guide
+
+1. Clone the repository locally
+2. Install Manim in a virtual environment (use the instructions found above)
+3. Run virtual environment
+   
+   ```
+      $ source venv/bin/activate venv
+   ```
+   
+4. Run the following command to create the Hypergraph Example
+
+    ```
+    $ manim -qm src/hypergraphs/hypergraphs.py HypergraphExample
+    ```
+   
+5. The mp4 will autoplay. If not please find the video in the following directory:
+
+   ``` 
+   $ media/videos/hypergraphs/720p30/
+   
+   ```
+
+## TL;DC (Too Long; Didn't Clone)
+
+Please find a collab workbook here:
+
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/nhsx/hypergraphical/blob/main/hypergraphs/workbooks/hypergraph-animations.ipynb)
+
---
+++ src/hypergraphs/hypergraphs.py
@@ -0,0 +1,111 @@
+from abc import ABC
+
+from manim import *
+
+
+class OuterLabeledDot(VGroup, ABC):
+    def __init__(self, position=ORIGIN, swatch=WHITE, label="", **kwargs):
+        dot = Dot(position, color=swatch)
+        super().__init__(dot, Tex(label).next_to(dot, 0.4 * UL), **kwargs)
+
+
+class HypergraphExample(ThreeDScene):
+    def construct(self):
+        # Transition 1 - Introducing a hypergraph
+        title = Tex(r"We can take a hypergraph")
+        title.to_corner(UP + LEFT)
+        d1 = OuterLabeledDot([1, 2, 0], label="D1", swatch=YELLOW),
+        d2 = OuterLabeledDot([-1, 1, 0], label="D2", swatch=BLUE),
+        d3 = OuterLabeledDot([1.4, -1.4, 0], label="D3", swatch=GREEN),
+        d4 = OuterLabeledDot([-3, -2, 0], label="D4", swatch=GREY),
+        d5 = OuterLabeledDot([3, 1, 0], label="D5", swatch=MAROON)
+
+        disease = [d1[0], d2[0], d3[0], d4[0], d5]
+
+        a1 = ArcPolygon([-4, -3, 1], [3, -2, 1], [-2, 2, 1])
+        a2 = ArcPolygon([1, -2, 0.5], [4, 2, 1], [0, 3, 1], color=RED)
+
+        edge1 = LabeledDot(Tex("e1", color=BLACK))
+        edge2 = LabeledDot(Tex("e2", color=BLACK))
+
+        self.play(
+            Write(title),
+        )
+
+        self.wait()
+
+        self.play(
+            FadeIn(*disease)
+        )
+
+        self.wait()
+
+        edge1.next_to(a1)
+        edge2.next_to(a2)
+
+        self.play(
+            Create(a1),
+            Create(a2),
+            FadeIn(edge1, edge2)
+        )
+
+        self.wait()
+
+        self.play(
+            FadeOut(
+                title,
+                *disease,
+                a1,
+                a2
+            )
+        )
+
+        incidence_matrix = IntegerMatrix([[0, 1],
+                                          [1, 0],
+                                          [1, 1],
+                                          [1, 0],
+                                          [0, 1]],
+                                         left_bracket="(",
+                                         right_bracket=")")
+
+        incidence_text = Tex(r"And convert it to an incidence matrix and a bipartite graph.")
+        incidence_text.to_corner(UP + LEFT)
+
+        self.play(
+            FadeIn(incidence_text),
+        )
+
+        self.wait()
+
+        self.add(edge2, incidence_matrix)
+
+        self.wait()
+
+        d = VGroup()
+        d.add(*disease)
+
+        l1 = Line(edge1, d[1])
+        l2 = Line(edge1, d[2])
+        l3 = Line(edge1, d[3])
+        l4 = Line(edge2, d[2])
+        l5 = Line(edge2, d[4])
+        l6 = Line(edge2, d[0])
+
+        self.play(FadeOut(incidence_text))
+
+        self.wait()
+
+        self.play(
+            incidence_matrix.animate.shift(LEFT * 5.5, UP),
+            FadeIn(*disease, a1, a2)
+        )
+
+        self.add(l1, l2, l3, l4, l5, l6)
+
+        self.wait()
+
+        self.play(
+            FadeOut(a1, a2)
+        )
+
+        self.wait()
