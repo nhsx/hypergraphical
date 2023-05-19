f4418bc2c937c211809fbba52cc6443ed9e0ccde
ZoeHancox <83767921+ZoeHancox@users.noreply.github.com>
2023-05-19 13:04:37 +0100

    Formatted code

--- src/create_figs.py
+++ src/create_figs.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
-import matplotlib
-import numpy as np
-import pandas as pd
-import seaborn as sns
+
+# import matplotlib
+# import numpy as np
+# import pandas as pd
+# import seaborn as sns
 
 
 def pagerank_scatter(suc_col, pred_col, dis_col, tab):
     """
     Create a scatterplot comparing successor PageRank to predecessor PageRank
-    to observe whether diseases are more likely to be a successor or predecessor.
+    to observe whether diseases are more likely to be a successor or
+    predecessor.
 
     Args:
         suc_col (df col) : PageRank successor column from dataframe.
@@ -29,14 +31,16 @@ def pagerank_scatter(suc_col, pred_col, dis_col, tab):
     y = [-0.01, 0.99, 0.99]
 
     ax.fill(x, y, color="orange", alpha=0.1)
-    ax.text(0.001, pred_col.max() + 0.04, "Predecessor", color="orange", fontsize=5)
+    pred_loc_y = pred_col.max() + 0.04
+    ax.text(0.001, pred_loc_y, "Predecessor", color="orange", fontsize=5)
 
     ax.fill(y, x, color="green", alpha=0.1)
-    ax.text(
-        suc_col.max() + 0.06, 0.01, "Successor", color="green", rotation=90, fontsize=5
-    )
+    succ_loc_x = suc_col.max() + 0.06
+    suc = "Successor"
+    ax.text(succ_loc_x, 0.01, suc, color="green", rotation=90, fontsize=5)
 
-    ax.scatter(x=suc_col, y=pred_col, s=200, c=range(0, len(dis_col)), alpha=0.5)
+    scat_cols = range(0, len(dis_col))
+    ax.scatter(x=suc_col, y=pred_col, s=200, c=scat_cols, alpha=0.5)
 
     for x, y, label in zip(suc_col, pred_col, dis_col):
         ax.text(x, y, label, va="center", ha="center", fontsize=6)
